b4500df9567b9b91846dd4089283d7f164865161
Diegand10 <90287105+Diegand10@users.noreply.github.com>
2021-10-22 17:43:11 -0500

    Add files via upload

---
+++ javascript2.js
@@ -0,0 +1,22 @@
+let array = [`Cristian`, `Karina`, `Maria`, `Diego`, `Karim`, `Karent`, `Sofia`, `Aura`, `David`, `Camilo`, `Ivan`, `Simon`, `Juan`, `Andres`, `Sebastian`]
+let conteo = array.length
+
+
+
+
+for (let index = 0; index < conteo; index++) {
+    document.write(array[index]);
+}
+
+ 
+
+let nombres = [`Julian`, `Marlen`, `Jimmy`, `Santiago`, `Yamile`, `Jose`, `Luis`, `Alejandra`, `Daniel`, `Ricardo`, `Leonardo`, `Laura`, `Tatiana`, `Gloria`, `Francisco`]
+
+while (nombres) {
+    document.write(nombres)
+    nombres++
+}
+
+
+
+
